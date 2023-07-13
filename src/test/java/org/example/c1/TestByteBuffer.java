93d3b84951b4db38f521fd8d95df7204ff6fe3f1
齐向前 <812573459@qq.com>
2023-07-13 15:16:52 +0800

    add:认识ByteBuffer

---
+++ src/test/java/org/example/c1/TestByteBuffer.java
@@ -0,0 +1,42 @@
+package org.example.c1;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * @className: TestByteBuffer
+ * @author: 齐向前
+ * @date: 2023/7/13
+ **/
+@Slf4j
+public class TestByteBuffer {
+
+    public static void main(String[] args) {
+        // FileChannel
+        // 1.输入输出流. 2. RandomAccessFile
+        try (FileChannel channel = new FileInputStream("data.txt").getChannel()) {
+            // 准备缓冲区
+            ByteBuffer buffer = ByteBuffer.allocate(10);
+            while (true) {
+                // 从channel读取数据,向buffer写入
+                int len = channel.read(buffer);
+                log.debug("字节数:{}", len);
+                if (len == -1) { // 没有内容了
+                    break;
+                }
+                // 打印buffer的内容
+                buffer.flip(); // 切换至读模式
+                while (buffer.hasRemaining()) { // 是否还有剩余未读数据
+                    byte b = buffer.get();
+                    log.debug("实际字节:{}", (char) b);
+                }
+                buffer.clear(); // 切换为写模式
+            }
+        } catch (IOException e) {
+        }
+    }
+}
