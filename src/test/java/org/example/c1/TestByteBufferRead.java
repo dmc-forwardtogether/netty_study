fc36e4ae7b6f626d7f6a48eb4169eb1492549ef4
齐向前 <812573459@qq.com>
2023-07-14 16:21:21 +0800

    add:ByteBuffer重复读

---
+++ src/test/java/org/example/c1/TestByteBufferRead.java
@@ -0,0 +1,41 @@
+package org.example.c1;
+
+import java.nio.ByteBuffer;
+
+import static org.example.c1.ByteBufferUtil.debugAll;
+
+
+/**
+ * @className: TestByteBufferRead
+ * @author: 齐向前
+ * @date: 2023/7/14
+ **/
+public class TestByteBufferRead {
+
+    public static void main(String[] args) {
+        ByteBuffer buffer = ByteBuffer.allocate(10);
+        buffer.put(new byte[]{'a', 'b', 'c', 'd'});
+        buffer.flip();
+
+        // rewind 从头开始读
+//        buffer.get(new byte[4]);
+//        debugAll(buffer);
+//        buffer.rewind();
+//        System.out.println((char) buffer.get());
+
+        // mark & reset
+        // mark 做一个标记,记录 position 位置, reset 是将 position 重置到 mark 的位置
+//        System.out.println((char) buffer.get());
+//        System.out.println((char) buffer.get());
+//        buffer.mark(); // 加标记,索引2的位置
+//        System.out.println((char) buffer.get());
+//        System.out.println((char) buffer.get());
+//        buffer.reset(); // 将 position 重置到索引2
+//        System.out.println((char) buffer.get());
+//        System.out.println((char) buffer.get());
+
+        // get(i)
+        System.out.println((char) buffer.get(3));
+        debugAll(buffer);
+    }
+}
